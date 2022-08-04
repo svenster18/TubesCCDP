e03cdedd3f80c998eb1ab9d8f15e1d7243ecfd65
svenster18 <mohamadrizki8@gmail.com>
2022-08-04 12:39:43 +0700

    create ticket and ticket builder

---
+++ Creational/Builder/Ticket.php
@@ -0,0 +1,40 @@
+<?php 
+    class Ticket {
+        private $schedule;
+        private $seats;
+        private $person;
+        private $paymentMethod;
+
+        public function setSchedule($schedule){
+            $this->schedule = $schedule;
+        }
+
+        public function getSchedule(){
+            return $this->schedule;
+        }
+
+        public function setSeats($seats){
+            $this->seats = $seats;
+        }
+
+        public function getSeats(){
+            return $this->seats;
+        }
+
+        public function setPerson($person){
+            $this->person = $person;
+        }
+
+        public function getPerson(){
+            return $this->person;
+        }
+
+        public function setPaymentMethod($paymentMethod){
+            $this->paymentMethod = $paymentMethod;
+        }
+
+        public function getPaymentMethod(){
+            return $this->paymentMethod;
+        }
+    }
+?>
---
+++ Creational/Builder/TicketBuilder.php
@@ -0,0 +1,27 @@
+<?php 
+
+    require_once('Ticket.php');
+
+    class TicketBuilder implements Builder {
+        private $ticket;
+
+        public function reset() {
+            $this->ticket = new Ticket();
+        }
+        public function setSchedule($schedule) {
+            $this->ticket->setSchedule($schedule);
+        }
+        public function setSeats($seats) {
+            $this->ticket->setSeats($seats);
+        }
+        public function setPerson($person){
+            $this->ticket->setPerson($person);
+        }
+        public function setPaymentMethod($paymentMethod){
+            $this->ticket->setPaymentMethod($paymentMethod);
+        }
+        public function print(): Ticket {
+            return $this->ticket;
+        }
+    }
+?>
