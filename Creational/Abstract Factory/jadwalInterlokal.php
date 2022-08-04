389baab91db6bf92d3e62ee59baa91b9698a0ff7
kajafxluvels <elsapriliashawty@gmail.com>
2022-08-05 02:05:10 +0700

    Create jadwalInterlokal.php

---
+++ Creational/Abstract Factory/jadwalInterlokal.php
@@ -0,0 +1,8 @@
+<?php
+    class JadwalInterlokal implements interlokal {
+        public function jadwalInterlokal()
+        {
+            Echo 'Surabaya(Argo Parahyangan) 18.00';
+        }
+    }
+?>
